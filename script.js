52eea3b29d104d6b459de8f651e389240241dd7e
siriprydz <siriprydz@gmail.com>
2025-10-05 11:50:56 +0200

    time calculations

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log(new Date(year, month, day));
+let countdown = document.querySelector(".countdown");
+console.log(countdown);
+
+let halloween = new Date(2025, 9, 31);
+let today = new Date();
+let timeLeft = halloween - today;
+
+console.log(timeLeft);
+console.log(halloween);
+console.log(today);
+
+let millisecondsPerSecond = 1000;
+let totalSeconds = Math.floor(timeLeft / millisecondsPerSecond);
+let secondsPerDay = 24 * 60 * 60;
+let days = Math.floor(totalSeconds / secondsPerDay);
+let remainingSecondsAfterDays = totalSeconds % secondsPerDay;
+let secondsPerHour = 60 * 60;
+let hours = Math.floor(remainingSecondsAfterDays / secondsPerHour);
+let remainingSecondsAfterHours = remainingSecondsAfterDays % secondsPerHour;
+let secondsPerMinute = 60;
+let minutes = Math.floor(remainingSecondsAfterHours / secondsPerMinute);
+let seconds = remainingSecondsAfterHours % secondsPerMinute;
+
